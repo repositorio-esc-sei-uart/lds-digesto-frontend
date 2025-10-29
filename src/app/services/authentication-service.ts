c11824c0e45670d91e5f419f984e1cc75979571d
Esteban <avilaesteban571@gmail.com>
2025-10-29 01:00:11 -0300

    Integracion del backend con el frontend para la creacion de usuarios

--- src/app/services/authentication-service.ts
+++ src/app/services/authentication-service.ts
@@ -30,37 +30,37 @@ export class AuthenticationService {
 
   // Base de datos simulada con diferentes roles y estados.
   private mockUserDatabase: MockUser[] = [
-    { id: 1, nombre: 'Admin',
-       apellido: 'Dev',
-       email: 'admin@unpa.edu.ar',
-       legajo: 'admin',
-       password: 'admin',
-       rol: { "idRol": 1, "nombre": "Administrador", "descripcion": "Usuario con acceso total al sistema."},
-       estado: { "idEstadoU": 1, "nombre": "Activo", "descripcion": "La cuenta del usuario está plenamente operativa y en uso."}
+    { idUsuario: 1, nombre: 'Admin',
+       apellido: 'Dev', 
+       email: 'admin@unpa.edu.ar', 
+       legajo: 'admin', 
+       password: 'admin', 
+       rol: { "idRol": 1, "nombre": "Administrador", "descripcion": "Usuario con acceso total al sistema."}, 
+       estadoU: { "idEstadoU": 1, "nombre": "Activo", "descripcion": "La cuenta del usuario está plenamente operativa y en uso."}
     },
-    { id: 2, nombre: 'Editor',
-      apellido: 'Dev',
-      email: 'editor@unpa.edu.ar',
-      legajo: 'editor',
-      password: 'editor',
-      rol: {"idRol": 2, "nombre": "Editor", "descripcion": "Usuario encargado de la gestión de documentos." },
-      estado: { "idEstadoU": 1, "nombre": "Activo", "descripcion": "La cuenta del usuario está plenamente operativa y en uso." }
+    { idUsuario: 2, nombre: 'Editor', 
+      apellido: 'Dev', 
+      email: 'editor@unpa.edu.ar', 
+      legajo: 'editor', 
+      password: 'editor', 
+      rol: {"idRol": 2, "nombre": "Editor", "descripcion": "Usuario encargado de la gestión de documentos." }, 
+      estadoU: { "idEstadoU": 1, "nombre": "Activo", "descripcion": "La cuenta del usuario está plenamente operativa y en uso." }
     },
-    { id: 3,
-      nombre: 'Jorgito',
+    { idUsuario: 3, 
+      nombre: 'Jorgito', 
       apellido: 'Gpt',
-       email: 'editor@unpa.edu.ar',
-       legajo: 'jorgito',
+       email: 'editor@unpa.edu.ar', 
+       legajo: 'jorgito', 
        password: 'jorgito',
-        rol:  { "idRol": 3, "nombre": "Lector", "descripcion": "Usuario encargado de Leer."},
-        estado: {"idEstadoU": 2, "nombre": "Inactivo","descripcion": "La cuenta del usuario desactivada."}
+        rol:  { "idRol": 3, "nombre": "Lector", "descripcion": "Usuario encargado de Leer."}, 
+        estadoU: {"idEstadoU": 2, "nombre": "Inactivo","descripcion": "La cuenta del usuario desactivada."}
     }
   ];
 
   constructor(private http: HttpClient) {
     // Al iniciar el servicio, intenta cargar al usuario desde localStorage.
     // Esto mantiene la sesión activa si el usuario refresca la página.
-    const storedUser = sessionStorage.getItem('currentUser');
+    const storedUser = localStorage.getItem('currentUser');
     if (storedUser) {
       this.currentUserSubject.next(JSON.parse(storedUser));
     }
@@ -89,7 +89,7 @@ export class AuthenticationService {
       tap(response => {
         if (response.success && response.user) {
           // Si el login es exitoso, guardamos en localStorage y ANUNCIAMOS el nuevo usuario.
-          sessionStorage.setItem('currentUser', JSON.stringify(response.user));
+          localStorage.setItem('currentUser', JSON.stringify(response.user));
           this.currentUserSubject.next(response.user);
         }
       })
@@ -98,7 +98,7 @@ export class AuthenticationService {
 
   logout(): void {
     // Se elimina al usuario de localStorage y se ANUNCIA que ya no hay nadie logueado.
-    sessionStorage.removeItem('currentUser');
+    localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
   }
 
@@ -129,8 +129,8 @@ export class AuthenticationService {
     }
 
     // Caso 2: Usuario encontrado, se verifica su estado.
-    if (userFound.estado?.nombre !== 'Activo') {
-      const response: AuthResponse = { success: false, message: `Acceso denegado. Su cuenta se encuentra en estado: ${userFound.estado}.` };
+    if (userFound.estadoU?.nombre !== 'Activo') {
+      const response: AuthResponse = { success: false, message: `Acceso denegado. Su cuenta se encuentra en estado: ${userFound.estadoU}.` };
       return of(response).pipe(delay(1500));
     }
 
@@ -139,7 +139,7 @@ export class AuthenticationService {
     const response: AuthResponse = {
       success: true,
       message: 'Login exitoso.',
-      token: `fake-jwt-token-for-${userProfile.rol?.nombre.toLowerCase()}-${userProfile.id}`,
+      token: `fake-jwt-token-for-${userProfile.rol?.nombre.toLowerCase()}-${userProfile.idUsuario}`,
       user: userProfile
     };
     return of(response).pipe(delay(1500));
